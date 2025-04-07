3794c2ea05ba796d68f2a3dc21d3466d52b2c98b
NarShaddaa <radius-unos@mail.ru>
2025-04-07 21:01:19 +0300

    RW1.1_SPRNT4_fnl

--- robot_platform.py
+++ robot_platform.py
@@ -1,27 +1,28 @@
 def min_platforms(weights: list[int], limit: int) -> int:
-    # Сортируем массив весов для двух указателей
-    weights.sort()
+    """Вычисляет минимальное количество платформ,
+    необходимых для размещения роботов."""
+    weights = sorted(weights)
 
     # Два указателя:
     # left - начало массива (самые легкие роботы)
     # right - конец массива (самые тяжелые роботы)
-    left = 0
-    right = len(weights) - 1
+    left_index = 0
+    right_index = len(weights) - 1
 
     # Счетчик необходимых платформ
     platforms = 0
 
     # Пока есть роботы для обработки (левая граница не перешла правую)
-    while left <= right:
+    while left_index <= right_index:
         # сажаем на одну платформу самого легкого и самого тяжелого
-        if weights[left] + weights[right] <= limit:
+        if weights[left_index] + weights[right_index] <= limit:
             # Если их суммарный вес допустим, берем обоих
             # и переходим к следующему легкому роботу
-            left += 1
+            left_index += 1
 
         # В любом случае самого тяжелого робота сажаем на платформу:
         # либо с самым легким (если влез), либо одного (если не влез)
-        right -= 1
+        right_index -= 1
 
         # Увеличиваем счетчик платформ после каждой итерации
         platforms += 1
@@ -29,12 +30,9 @@ def min_platforms(weights: list[int], limit: int) -> int:
     return platforms
 
 
-# Входные данные
-weights = list(map(int, input().split()))  # Массив весов роботов
-limit = int(input())                       # Грузоподъемность платформы
+if __name__ == "__main__":
+    weights = [int(x) for x in input().split()]  # Массив весов роботов
+    limit = int(input())                       # Грузоподъемность платформы
 
-# Результат
-platforms = min_platforms(weights, limit)
-
-# Вывод целого числа
-print(platforms)
+    # Результат
+    print(min_platforms(weights, limit))
